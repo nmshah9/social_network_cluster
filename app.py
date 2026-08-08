bad30286ded4f535095ec787ca28fc973daedb37
nmshah9 <nmshah9@gmail.com>
2026-08-08 18:11:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -446,9 +446,8 @@ elif page == "Predict a Student":
                     top_words = profile.loc[cluster_pred].sort_values(ascending=False).head(8)
                     st.bar_chart(top_words)
 
-# Check if trained model exists
-if not os.path.exists("models/student_segment_model.pkl"):
-    subprocess.run(["python", "run_pipeline.py"])
+# postBuild
+python run_pipeline.py
 
 # ============================================================
 # FOOTER
